49b737ca346ab368bed4709afab4e6a3f987de54
Ram Niwash <rniwash122@gmai.com>
2020-12-08 11:14:04 +0530

    Spring framework demo project created

---
+++ src/main/java/com/ram/demo/spring/core/App.java
@@ -0,0 +1,12 @@
+package com.ram.demo.spring.core;
+
+/**
+ * 
+ * @author Ram Niwash
+ *	
+ */
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
